28a568541007f19616c0e43101f5a53e74a3ca89
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-01-02 17:08:21 +0000

    Convert button works and can read user input

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const getUserInput = document.getElementById("user-input")
+const convertBtn = document.getElementById("convertBtn")
+let userInput = getUserInput
+
+
+convertBtn.addEventListener("click", function(){
+    console.log(userInput.value)
+})
