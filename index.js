2d50168e14ddc995fe1452b9adfb133f0987ab23
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-01-02 20:23:05 +0000

    calculations added

--- index.js
+++ index.js
@@ -12,22 +12,35 @@ const conversionParas = document.querySelectorAll('.unit-measured');
 
 
 convertBtn.addEventListener("click", function(){
-    conversionParas[0].innerHTML = `<p>${userInput.value} meters = ${(metersToFeet().toFixed(2))} feet | ${userInput.value} feet = ${feetToMeters().toFixed(2)} meters</p>`
-    conversionParas[1].innerHTML += `<p>${userInput.value} litres = ${litresToGallons()} gallons | 20 feet = 6.096 meter</p>`
-    conversionParas[2].innerHTML += `<p>20 meters = 65 feet | 20 feet = 6.096 meter</p>`
+    const input = Number(userInput.value)
+    conversionParas[0].innerHTML = `<p>${input} meters = ${(metersToFeet(input).toFixed(2))} feet | ${input} feet = ${feetToMeters(input).toFixed(2)} meters</p>`
+    conversionParas[1].innerHTML += `<p>${input} litres = ${litresToGallons(input)} gallons | ${input} gallons = ${gallonsToLitres(input)} litres</p>`
+    conversionParas[2].innerHTML += `<p>${input} kilos = ${kilosToPounds(input)} pounds | ${input} pounds = ${poundsToKilos(input)} kilos</p>`
 
 })
 
-function metersToFeet(){
-    return (userInput.value * 3.281)
+function metersToFeet(num){
+    return (num * 3.281)
 }
 
-function feetToMeters(){
-    return userInput.value * 0.3048
+function feetToMeters(num){
+    return num * 0.3048
 }
 
-function litresToGallons(){
-return userInput.value * 0.264
+function litresToGallons(num){
+return num * 0.264
+}
+
+function gallonsToLitres(num){
+    return num * 3.785
+}
+
+function kilosToPounds(num){
+return num * 2.205
+}
+
+function poundsToKilos(num) {
+    return num * 0.454;
 }
 
 
